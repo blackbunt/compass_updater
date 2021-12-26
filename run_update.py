4ef602a93981dc40707cace2290467f814abd834
balckbunt <blackbunt@posteo.se>
2021-12-26 13:07:22 +0100

    - Bug fixes in comp_update, liz_update

--- run_update.py
+++ run_update.py
@@ -13,18 +13,23 @@ def read_config(file_path):
 
 def compass_upd(folder: str):
     liste = []
+    files = []
     menu = 'Zurück zum Hauptmenü'
     os.chdir( folder )
     for file in glob.glob( "*.exe" ):
         liste.append( file )
         # liste.append(file.split(".")[-2].replace("_", "."))
-    files = natsort.humansorted(liste, reverse=True)
-    filename = files[0]
-    files.insert(0, menu)
-    filename_bella = filename.split( "." )[-2].replace( "_", "." )
-    # create auswahl menu
-    title = f'Wähle Update aus. Neustes Update: {filename_bella}'
 
+    if len(liste) != 0:
+        files = natsort.humansorted(liste, reverse=True)
+        filename = files[0]
+        filename_bella = filename.split(".")[-2].replace("_", ".")
+        files.insert(0, menu)
+        title = f'Wähle Update aus. Neustes Update: {filename_bella}'
+    else:
+        files.insert(0, menu)
+        title = 'Kein Compass Update heruntergeladen.'
+    # create auswahl menu
     option, index = pick( files, title, indicator='=>', default_index=0 )
     if index == 0:
         return None
@@ -34,19 +39,28 @@ def compass_upd(folder: str):
 
 def liz_update(folder: str):
     liste = []
-    os.chdir( folder )
-    for file in glob.glob( "*.exe" ):
-        liste.append( file )
+    files = []
+    menu = 'Zurück zum Hauptmenü'
+    os.chdir(folder)
+    for file in glob.glob("*.exe"):
+        liste.append(file)
         # liste.append(file.split(".")[-2].replace("_", "."))
-    files = natsort.humansorted( liste, reverse=True )
-    filename = files[0]
-    filename_bella = filename.split( "." )[-2].replace( "_", "." )
+
+    if len(liste) != 0:
+        files = natsort.humansorted(liste, reverse=True)
+        filename = files[0]
+        filename_bella = filename.split(".")[-2].replace("_", ".")
+        files.insert(0, menu)
+        title = f'Wähle Update aus. Neustes Update: {filename_bella}'
+    else:
+        files.insert(0, menu)
+        title = 'Kein Lizenzstecker Update heruntergeladen.'
     # create auswahl menu
-    title = f'Wähle Update aus. Neustes Update: {filename_bella}'
-
-    option, index = pick( files, title, indicator='=>', default_index=0 )
-    subprocess.call( filename )
-
+    option, index = pick(files, title, indicator='=>', default_index=0)
+    if index == 0:
+        return None
+    else:
+        subprocess.call(os.path.join(folder, option))
 
 if __name__ == '__main__':
     config = read_config( 'config.yaml' )
