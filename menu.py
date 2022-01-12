62d0be336fa3e3a5a57cc452c70a293ce8eea239
blackbunt@posteo.se <$n3qLBaPVZO!a#!5z0%TS8!cPyRdTCqN%zzDsTr7y2FwVpTRP7jrIq@lBDChM4rGN2OM*xj6>
2022-01-12 18:29:26 +0100

    added yes/no choice menu

--- menu.py
+++ menu.py
@@ -1,11 +1,24 @@
 from pick import pick
 
 
-def show_menu():
+def show_main_menu():
     title = 'Compass Update Helper: '
     options = ['Starte Update', 'Downloade Update', 'Starte Lizenzstecker Update', 'Beenden']
 
     option, index = pick(options, title, indicator='•', default_index=0)
 
     return [option, index]
+
+
+def menu_yes_no(message: str):
+    """
+    Renders a yes/no option menu, returns choice
+    :param message: message printed for decision
+    :return:
+    """
+    options = ['Ja', 'Nein']
+    title = message
+
+    option, index = pick(options, title, indicator='•', default_index=1)
+    return option, index
 
